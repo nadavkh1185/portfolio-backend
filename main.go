309005c5e9515f6fb426b9ac454391244f98def0
nadavkh1185 <nadavkh1185@gmail.com>
2026-04-27 12:04:35 +0700

    add vercel domain

--- main.go
+++ main.go
@@ -30,6 +30,7 @@ func main() {
 	allowedOrigins := []string{
 		"http://localhost:3000",
 		"http://127.0.0.1:3000",
+		"https://portfolio-frontend-tan-three.vercel.app",
 	}
 
 	if frontendURL := strings.TrimSpace(os.Getenv("FRONTEND_URL")); frontendURL != "" {
